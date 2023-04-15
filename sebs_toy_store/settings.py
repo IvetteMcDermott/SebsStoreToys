4a82868ddc89f7e6d50d6f91db706e88cf269460
IvetteMcDermott <>
2023-04-15 18:25:37 +0000

    :rocket: Settings: Debug to False

--- sebs_toy_store/settings.py
+++ sebs_toy_store/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sebs-toy-store.herokuapp.com','8000-ivettemcdermott-sebsstor-soo8sumjem.us2.codeanyapp.com']
 
