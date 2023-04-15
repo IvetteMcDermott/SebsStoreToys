14c3540efd3c8ae63da114a4cdf2849ae563875b
IvetteMcDermott <>
2023-04-15 16:11:32 +0000

    :wrench: Store App: Admin Site registration

--- sts_store/admin.py
+++ sts_store/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Ware, Category, Subcategory, WareImage
 
 # Register your models here.
+
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_filter = ('name',)
+    list_display = ('name',)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+@admin.register(Subcategory)
+class Subcategory(admin.ModelAdmin):
+    list_filter = ('name',)
+    list_display = ('name', 'category',)
+
+    class Meta:
+        verbose_name_plural = 'Subcategories'
+
+
+@admin.register(Ware)
+class Ware(admin.ModelAdmin):
+    list_filter = ('subcategory',)
+    search_fields = ['subcategory', 'name']
+    list_display = ('name', 'sku', 'description', 'subcategory', 'price',
+                    'height', 'width', 'length', 'new', 'clearance', )
+
+
+@admin.register(WareImage)
+class WareImage(admin.ModelAdmin):
+    list_filter = ('ware',)
+    search_fields = ['ware']
+    list_display = ('ware', 'image', 'caption',)
