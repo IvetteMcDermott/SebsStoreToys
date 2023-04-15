6710e738a4ab131ec52b495cd163d078454e6f6d
IvetteMcDermott <>
2023-04-15 16:10:28 +0000

    :card_file_box: Store App: Add incoming field and migration

--- sts_store/models.py
+++ sts_store/models.py
@@ -21,7 +21,7 @@ class Subcategory(models.Model):
 
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.CASCADE,
-                               default='category')
+                                 default='category')
 
     def __str__(self):
         return self.name
@@ -35,11 +35,12 @@ class Ware(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     new = models.BooleanField(default=False, null=True, blank=True)
     clearance = models.BooleanField(default=False, null=True, blank=True)
+    incomming = models.BooleanField(default=False, null=True, blank=True)
     subcategory = models.ForeignKey(Subcategory, on_delete=models.CASCADE,
-                               default='subcategory')
-    height =  models.IntegerField(null=True, blank=True)
-    width =  models.IntegerField(null=True, blank=True)
-    length =  models.IntegerField(null=True, blank=True)
+                                    default='subcategory')
+    height = models.IntegerField(null=True, blank=True)
+    width = models.IntegerField(null=True, blank=True)
+    length = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -47,9 +48,8 @@ class Ware(models.Model):
 
 class WareImage(models.Model):
     ware = models.ForeignKey(Ware, on_delete=models.CASCADE,
-                               default='ware_image')
+                             default='ware_image')
     image = CloudinaryField(default='placeholder', max_length=255,
                             verbose_name='image')
     caption = models.CharField(max_length=254, null=False, blank=False)
-
 
