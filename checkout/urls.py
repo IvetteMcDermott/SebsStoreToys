7e9f8ce9263ae3dc04df5023b1d30023800af4a6
IvetteMcDermott <>
2023-04-21 11:36:30 +0000

    :construction: Checkout App:Add code & models to set functionality of it
    Adapted code from Ado repo.

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,6 @@ app_name = 'checkout'
 
 
 urlpatterns = [
+    path('', views.checkout, name='checkout'),
 
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from .forms import OrderForm
 
-# Create your views here.
+import stripe
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+        'cart': cart,
+    }
+
+    return render(request, 'checkout/checkout.html', context)
