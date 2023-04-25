0bda6d3c3a788906c92563052c8dd22640b55d9c
IvetteMcDermott <>
2023-04-25 20:32:17 +0000

    :construction: Profiles App: Add for show the orders query

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import ProfileForm
+from checkout.models import Order
 from django.views import generic, View
 from django.contrib.auth.decorators import login_required
+
+from django.contrib import messages
+
+from django.http import HttpResponse
 
 
 # Create your views here.
@@ -15,13 +20,24 @@ def my_profile(request):
     """ THE PROFILE IS CREATED BY SIGNAL, SO DOESNT HAVE ANYOTHER """
     """ INFORMATION THAN THE USER """
     user = get_object_or_404(UserProfile, user=request.user)
+    orders = user.orders.all()
+    print(orders)
+    template = 'profiles/user_profile.html'
+
     if request.method == 'POST':
         form = ProfileForm(request.POST, request.FILES, instance=user)
 
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile had been updated successfully!')
-        else:
-            form = ProfileForm(instance=user)
-    return render(request, 'profiles/user_profile.html', {'form': form,
-                  'user': user, })
+    else:
+        form = ProfileForm(instance=user)
+
+    context = {
+                'form': form,
+                'user': user,
+                'orders': orders,
+                }
+
+    return render(request, template, context)
+
