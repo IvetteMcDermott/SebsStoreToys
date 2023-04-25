cbd302d0bbc7929a0745e87a542d32cf66b81491
IvetteMcDermott <>
2023-04-25 11:35:02 +0000

    :ambulance: Fixes to get a new space up after remove vscode folder

--- sebs_toy_store/settings.py
+++ sebs_toy_store/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sebs-toy-store.herokuapp.com', '8000-ivettemcdermott-sebsstor-soo8sumjem.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['sebs-toy-store.herokuapp.com', '8000-ivettemcdermott-sebsstor-b7hf2ylbyf.us2.codeanyapp.com']
 
 
 # Application definition
