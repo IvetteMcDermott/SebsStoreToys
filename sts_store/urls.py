a3f4dc632c77b2000809d2057b20776876165a2b
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-05-09 18:31:05 +0000

    :sparkles: Store App:Add Staff Panel features

--- sts_store/urls.py
+++ sts_store/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('ware/<int:ware_id>/', views.WareDetail.as_view(), name='ware_detail'),
     path('<int:ware_id>/wareedit/', views.WareEdit, name='ware_edit'),
     path('<int:ware_id>/waredelete/', views.WareDelete, name='ware_delete'),
+    path('<int:image_id>/imagedelete/', views.image_delete, name='image_delete'),
     path('storepanel/', views.StorePanel, name='store_panel'),
     path('search/', views.search, name='search'),
     path('wareentry/', views.WareEntry.as_view(), name='ware_entry'),
-    path('imageentry/', views.ImageForm.as_view(), name='image_entry'),
+    path('imageentry/', views.AddImageForm.as_view(), name='image_entry'),
+    path('orders/', views.orders_list, name='orders'),
 ]
--- sts_store/views.py
+++ sts_store/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import generic, View
 from django.views.generic import UpdateView, ListView, CreateView
 from .models import Ware, WareImage, Category, Subcategory
+from checkout.models import Order, OrderLineItem
 
 from django.contrib.messages.views import SuccessMessageMixin
 
@@ -47,6 +48,7 @@ def search(request):
     """ VIEW FOR SEARCH A PLACE POST """
     if request.method == 'POST':
         search = request.POST.get('q')
+        print(search)
         query = Ware.objects.all().filter(Q(name__icontains=search) | Q(description__icontains=search))
         template = 'store_list.html'
         context = {
@@ -64,19 +66,33 @@ def StorePanel(request):
 @method_decorator(staff_member_required, name='dispatch')
 class WareEntry(SuccessMessageMixin, CreateView):
     """ VIEW FOR ADD A NEW WARE """
-    template_name = 'admin/admin_page.html'
+    template_name = 'admin/add_ware.html'
     model = Ware
     form_class = WareForm
-    success_url = '/'
+
+    def get_success_url(self):
+        return self.request.path
 
 
 @method_decorator(staff_member_required, name='dispatch')
-class ImageForm(SuccessMessageMixin, CreateView):
+class AddImageForm(SuccessMessageMixin, CreateView):
     """ VIEW FOR ADD A NEW WARE """
-    template_name = 'admin/image_form.html'
+    template_name = 'admin/add_image_form.html'
     model = WareImage()
     form_class = ImageForm
-    success_url = '/'
+
+    def get_success_url(self):
+        return self.request.path
+
+
+@staff_member_required
+def image_delete(request, image_id):
+    """ VIEW FOR ADD A NEW WARE """
+    if request.method == 'GET':
+        image = WareImage.objects.get(image_id=pk)
+        image.delete()
+
+    return render(request, 'admin/store_panel.html')
 
 
 @staff_member_required
@@ -84,13 +100,12 @@ def WareEdit(request, ware_id):
     """ VIEW FOR ADD A NEW WARE """
     form = WareForm()
     ware = Ware.objects.get(id=ware_id)
-    print(ware)
     if request.method == 'POST':
         form = WareForm(request.POST, instance=ware)
         if form.is_valid():
             form.save(commit=False)
             form.save()
-            return render(request, 'admin/success.html')
+            return render(request, 'admin/store_panel.html')
     else:
         form = WareForm(instance=ware)
 
@@ -102,7 +117,22 @@ def WareDelete(request, ware_id):
     """ VIEW FOR ADD A NEW WARE """
     if request.method == 'GET':
         ware = Ware.objects.get(id=ware_id)
-        print(ware)
         ware.delete()
 
-    return render(request, 'admin/success.html')
+    return render(request, 'admin/store_panel.html')
+
+
+@staff_member_required
+def orders_list(request, *args, **kwargs):
+    """ TO SEARCH ORDERS BY DATE - ADMIN FEATURE """
+    """ TO SEARCH ORDERS BY USER - ADMIN FEATURE """
+    model = Order
+    orders = model.objects.all().order_by('-date')
+
+    return render(
+            request,
+            "admin/orders_list.html",
+            {
+               "orders": orders,
+            },
+        )
