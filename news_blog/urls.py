cbba8594be2d546c7be65742f220baa0dc7decc1
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-05-17 23:32:54 +0000

    :sparkles: NewsBlogApp: Add CRUD for admin

--- news_blog/urls.py
+++ news_blog/urls.py
@@ -6,6 +6,8 @@ app_name = 'news_blog'
 
 urlpatterns = [
     path('', views.NewsList.as_view(), name='news_list'),
-    path('newsentry/', views.NewsEntry.as_view(), name='news_entry'),\
+    path('addnews/', views.NewsEntry.as_view(), name='add_news'),
     path('news/<int:title_id>/', views.NewsDetails.as_view(), name='news_details'),
+    path('<int:title_id>/newsedit/', views.news_edit, name='news_edit'),
+    path('<int:title_id>/newsdelete/', views.news_delete, name='news_delete'),
 ]
--- news_blog/views.py
+++ news_blog/views.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib import messages
 
 from django.shortcuts import render, redirect, get_object_or_404
 
@@ -19,8 +20,8 @@ from .forms import NewsForm
 class NewsList(ListView):
     """ VIEW FOR LIST OF NEWS """
     model = NewsBlog
-    queryset = model.objects.all()
-    # paginate_by = 4
+    queryset = model.objects.all().order_by('-id')
+    paginate_by = 4
     template_name = "news_blog/news_list.html"
 
 
@@ -29,6 +30,7 @@ class NewsDetails(CreateView):
     RENDER THE DETAILS OF NEWS
     """
     model = NewsBlog
+    form = NewsForm()
 
     def get(self, request, title_id, *args, **kwargs):
         model = NewsBlog
@@ -53,3 +55,35 @@ class NewsEntry(SuccessMessageMixin, CreateView):
 
     def get_success_url(self):
         return self.request.path
+
+
+@staff_member_required
+def news_edit(request, title_id):
+    """ VIEW FOR ADD A NEW WARE """
+    form = NewsForm
+    new = get_object_or_404(NewsBlog, id=title_id)
+    template_name = 'admin/news_edit.html'
+
+    if request.method == 'POST':
+        form = NewsForm(request.POST, request.FILES, instance=new)
+
+        if form.is_valid():
+            form.save(commit=False)
+            form.save()
+            messages.success(request, 'The new had been EDIT successfully!')
+            return render(request, 'admin/staff_panel.html')
+    else:
+        form = NewsForm(instance=new)
+
+    return render(request, template_name, {'form': form, 'new': new})
+
+
+@staff_member_required
+def news_delete(request, title_id):
+    """ VIEW FOR ADD A NEW WARE """
+    if request.method == 'GET':
+        new = NewsBlog.objects.get(id=title_id)
+        new.delete()
+        messages.success(request, 'The new had been DELETE successfully!')
+
+    return render(request, 'admin/staff_panel.html')
